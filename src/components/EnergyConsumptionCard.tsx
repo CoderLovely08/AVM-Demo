4808cff4df7e9cb7790268f0c506c8f181a57d08
Lovely Sharma <lovelysingeras297@gmail.com>
2026-02-11 11:47:47 +0530

    [Feature]: Card title Test

--- src/components/EnergyConsumptionCard.tsx
+++ src/components/EnergyConsumptionCard.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import CardLayout from '../layout/CardLayout'
+import CardTitle from './custom/Cardtitle'
+import { flashIcon } from '../data'
 
 const EnergyConsumptionCard = () => {
   return (
     <CardLayout>
+      <div className="flex items-center justify-between">
+        <CardTitle icon={flashIcon} label="EnergyConsumptionCard" />
+        <span className="px-4 py-1.5 bg-slate-600/30 text-white text-sm rounded-full">
+          Yesterday
+        </span>
+      </div>
       <div>EnergyConsumptionCard</div>
     </CardLayout>
   )
