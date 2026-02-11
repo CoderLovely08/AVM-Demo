f9c6e69d56e4ddd127c81707d81fd33a65e7e16c
Lovely Sharma <lovelysingeras297@gmail.com>
2026-02-11 12:01:48 +0530

    [Feature]: Footfal base card

--- src/components/FootfallCard.tsx
+++ src/components/FootfallCard.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import CardLayout from '../layout/CardLayout'
+import CardTitle from './custom/Cardtitle'
+import { usersIcon } from '../data'
 
 const FootfallCard = () => {
+
   return (
     <CardLayout>
-      <div>FootfallCard</div>
+      <div className="flex items-center justify-between mb-8">
+        <CardTitle icon={usersIcon} label="Footfall" />
+        <span className="px-4 py-1.5 bg-[#1a3f3f] text-white text-sm rounded-full">
+          Yesterday
+        </span>
+      </div>
     </CardLayout>
   )
 }
