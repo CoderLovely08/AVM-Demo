94d95d258e6a0534fdc7c0c87499606c085fac6b
Lovely Sharma <lovelysingeras297@gmail.com>
2026-02-11 10:24:02 +0530

    [Feature]: Global imports

---
+++ src/data/index.js
@@ -0,0 +1,31 @@
+import logo from "/assets/ds-logo.svg";
+
+import homeIcon from "/assets/nav-icons/home.svg";
+import pieIcon from "/assets/nav-icons/pie.svg";
+import tilesIcon from "/assets/nav-icons/tiles.svg";
+
+import airIcon from "/assets/air.png";
+import dotsIcon from "/assets/dots.png";
+import dropIcon from "/assets/drop.png";
+import flashIcon from "/assets/flash.png";
+import humidIcon from "/assets/Humid.png";
+import lightIcon from "/assets/light.png";
+import usersIcon from "/assets/users.png";
+
+export {
+  // Logos
+  logo,
+  // Nav Icons
+  homeIcon,
+  pieIcon,
+  tilesIcon,
+
+  // Card Icons
+  airIcon,
+  dotsIcon,
+  dropIcon,
+  flashIcon,
+  humidIcon,
+  lightIcon,
+  usersIcon,
+};
